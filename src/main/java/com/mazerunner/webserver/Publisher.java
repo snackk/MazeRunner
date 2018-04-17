236de72c95fffa4351f287b258d892fd2876d84c
goncalochincho <goncalochincho@gmail.com>
2018-04-17 23:52:46 +0100

    added test handler, changed port to 8000

--- src/main/java/com/mazerunner/webserver/Publisher.java
+++ src/main/java/com/mazerunner/webserver/Publisher.java
@@ -2,6 +2,7 @@ package com.mazerunner.webserver;
 
 import com.mazerunner.webserver.handler.RegisterIpHandler;
 import com.mazerunner.webserver.handler.RequestHandler;
+import com.mazerunner.webserver.handler.TestHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.net.InetSocketAddress;
@@ -9,18 +10,20 @@ import java.util.concurrent.Executors;
 
 public class Publisher {
 
-    private static final int portNumber = 8080;
+    private static final int portNumber = 8000;
     private static final int threadsNumber = 20;
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(portNumber), 0);
 
-        server.createContext("/r.html", new RequestHandler());
+        server.createContext("/mzrun.html", new RequestHandler());
         server.createContext("/r_ip.html", new RegisterIpHandler());
+	server.createContext("/test.html",new TestHandler());
 
-        System.out.println("Publishing on: http://localhost:" + portNumber + "/r.html");
+        System.out.println("Publishing on: http://localhost:" + portNumber + "/mzrun.html");
         System.out.println("Publishing on: http://localhost:" + portNumber + "/r_ip.html");
-        server.setExecutor(Executors.newFixedThreadPool(threadsNumber));
+        System.out.println("Publishing on: http://localhost:" + portNumber + "/test.html");
+	server.setExecutor(Executors.newFixedThreadPool(threadsNumber));
         server.start();
     }
 }
