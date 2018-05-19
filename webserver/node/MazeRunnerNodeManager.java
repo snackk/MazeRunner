b4172ce97ad1f4beff58dcc18e3439357014e102
goncalochincho <goncalochincho@gmail.com>
2018-05-19 18:03:41 +0100

    closes #5

--- webserver/node/MazeRunnerNodeManager.java
+++ webserver/node/MazeRunnerNodeManager.java
@@ -13,6 +13,12 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
 
+import org.apache.commons.math3.stat.regression.OLSMultipleLinearRegression;
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.exception.MathIllegalArgumentException;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
 
 public class MazeRunnerNodeManager {
 
@@ -24,8 +30,11 @@ public class MazeRunnerNodeManager {
     /*Nodes*/
     private static Map<String, NodeInfo> nodesByIp = new HashMap<>();
     private static Long nodeMaxLoad = 90L;
-
-
+	 
+    private enum paramsType {x0, y0, x1, y1, v, s, m}
+    private enum Features {x0, y0, x1, y1, v}
+    private static final String LINEAR_REGRESSION_TARGET = "bbl";
+    
     private MSSManager mssmanager;
 
     protected MazeRunnerNodeManager(){ 
@@ -55,6 +64,8 @@ public class MazeRunnerNodeManager {
             throw new NoActiveNodesException();
         }
 
+	double predictedBBL = predictBBL(request);
+	System.out.println(predictedBBL);
         String workerIp = "";
         try {
             workerIp = loadBalanceRequest(request);
@@ -163,4 +174,61 @@ public class MazeRunnerNodeManager {
             nodesByIp.remove(downNode);
         }
     }
+
+   public double predictBBL(String request) {
+	Map<String,String> mapQuery = new HashMap<String, String>();
+	double[] requestArguments = new double[5];
+
+	int countArgs = 0;
+
+	for(String param : request.split("&")) {
+		String pair[] = param.split("=");	
+		if(pair.length > 1){ 
+			mapQuery.put(pair[0], pair[1]);
+			if(!pair[0].equals("s") && !pair[0].equals("m")){
+				requestArguments[countArgs] = Double.parseDouble(pair[1]);
+				countArgs++;
+			}	
+		}
+		else
+			mapQuery.put(pair[0], "");	
+	}	
+
+	ScanResult scanResult = mssmanager.fetchDB(mapQuery.get("s"), mapQuery.get("m"));
+	
+	OLSMultipleLinearRegression model = new OLSMultipleLinearRegression();
+	double[][] features = new double[scanResult.getCount()][Features.values().length]; 
+	double[] target = new double[scanResult.getCount()];        
+	double[] beta;
+	
+	RealMatrix coef;	
+	double predictedBBL = 0;
+
+	int countMap, countEnum;
+	countMap = countEnum = 0;
+
+	for (Map<String, AttributeValue> map : scanResult.getItems()) {
+		for (Features feature : Features.values()){
+			features[countMap][countEnum] = Double.parseDouble(map.get(feature.toString()).getS());
+			countEnum++;
+		}		
+		target[countMap] = Double.parseDouble(map.get(LINEAR_REGRESSION_TARGET).getS());
+		countEnum = 0;				
+		countMap ++;
+	}
+	try{
+		model.newSampleData(target, features);
+        	coef = MatrixUtils.createColumnRealMatrix(model.estimateRegressionParameters()); 	
+		beta = coef.getData()[0];
+
+		predictedBBL = beta[0];
+		for(int i = 0; i < requestArguments.length; i++)
+			predictedBBL += beta[i+1] * requestArguments[i];		
+   	
+		return predictedBBL;
+	} catch(MathIllegalArgumentException e) {
+		System.out.println("Not enough data yet...");
+	}
+	return -1.0;
+   }
 }
